a20f5c0bcc835559ed6e387d5621a78e16ec1e73
Javier9684 <javiersh@disroot.org>
2022-09-27 10:04:07 +0100

    Modificación de carpetas y he añadido codigo al index, new_reg.

---
+++ modules/include/news_reg.php
@@ -0,0 +1,7 @@
+<?php
+    require "../require/config.php";
+    $nombre_completo=$email=$numero_telefono=$dirrecion=$ciudad
+    =$comunidades=$c_postal=$check=$formato=$mensaje="";
+    
+    //if ()
+?>

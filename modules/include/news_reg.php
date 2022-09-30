9f9174b23d705075396911058bbbdfbdc796f05f
Javier9684 <javiersh@disroot.org>
2022-09-30 09:36:38 +0100

    Hemos limpiado las variables y con el metodo isset para quitar las abvertencias

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -30,7 +30,7 @@
 
 
     function validarTelefono($numero_telefono) {
-        if(!preg_match("/^[0-9]{10}+$/", $numero_telefono)) {
+        if(!preg_match("/^[0-9]{9}+$/", $numero_telefono)) {
             return false;
         } else {
             return true;
@@ -40,23 +40,53 @@
     //si llegan datos
     if ($_SERVER["REQUEST_METHOD"]=="POST") {
         print_r($_POST);
-     
+        //variables requeridas para enviar a BBDD
         if (!empty($_POST["nombre_completo"]) || !empty($_POST["email"]) || !empty($_POST["numero_telefono"])) {
-            
+
             $nombre_completo=limpiarDatos($_POST["nombre_completo"]);
-            echo "<br><strong>nombre: </strong>" . $nombre_completo . "<br>";
             $email=limpiarDatos($_POST["email"]);
-            echo "<strong>email: </strong>" . $email . "<br>";
             $numero_telefono=limpiarDatos($_POST["numero_telefono"]);
-            echo "<strong>telefono: </strong>" . $numero_telefono . "<br>";
-            $direccion=limpiarDatos($_POST["direccion"]);
-            $ciudad=limpiarDatos($_POST["ciudad"]);
-            $comunidades=limpiarDatos($_POST["comunidades"]);
-            $c_postal=limpiarDatos($_POST["c_postal"]);
-            $check=limpiarDatos($_POST["check"]);
-            $formato=limpiarDatos($_POST["formato"]);
-            $mensaje=limpiarDatos($_POST["mensaje"]);
-        
+            echo "<br><strong>Nombre: </strong>" . $nombre_completo . "<br>";
+            echo "<strong>Email: </strong>" . $email . "<br>";
+            echo "<strong>Telefono: </strong>" . $numero_telefono . "<br>";
+
+            if (isset($_POST["direccion"])) {
+                $direccion=limpiarDatos($_POST["direccion"]);
+            } else {
+                $direccion=null;
+            }
+            if (isset($_POST["ciudad"])) {
+                $ciudad=limpiarDatos($_POST["ciudad"]);
+            } else {
+                $ciudad=null;
+            }
+            if (isset($_POST["comunidades"])) {
+                $comunidades=limpiarDatos($_POST["comunidades"]);
+            } else {
+                $comunidades=null;
+            }
+            if (isset($_POST["c_postal"])) {
+                $c_postal=limpiarDatos($_POST["c_postal"]);
+            } else {
+                $c_postal=null;
+            }
+            if (isset($_POST["check"])) {
+                $check=limpiarDatos($_POST["check"]);
+            } else {
+                $check=null;
+            }
+            if (isset($_POST["formato"])) {
+                $formato=limpiarDatos($_POST["formato"]);
+            } else {
+                $formato=null;
+            }
+            if (isset($_POST["mensaje"])) {
+                $mensaje=limpiarDatos($_POST["mensaje"]);
+            } else {
+                $mensaje=null;
+            }
+            
+            //comprobar si valida estas variables
             if (validarNombre($nombre_completo)) {
                 echo "validado";
             } else {
